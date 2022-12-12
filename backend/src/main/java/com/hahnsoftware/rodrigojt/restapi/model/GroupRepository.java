a95def8073fc2a37666f1280f559d4e7213208be
Rodrigo Teixeira <rodrigojoseteixeira92@gmail.com>
2022-12-12 15:40:51 -0300

    Creating GroupRepository and Initializer classes

---
+++ backend/src/main/java/com/hahnsoftware/rodrigojt/restapi/model/GroupRepository.java
@@ -0,0 +1,9 @@
+package com.hahnsoftware.rodrigojt.restapi.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Group findByName(String name);
+}
