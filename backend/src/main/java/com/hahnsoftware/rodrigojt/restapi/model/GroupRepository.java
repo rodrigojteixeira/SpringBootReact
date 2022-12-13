ca378db164e9e541839f41baad7a8352d73caa0c
Rodrigo Teixeira <rodrigojoseteixeira92@gmail.com>
2022-12-13 11:42:57 -0300

    Creating GroupController class

--- backend/src/main/java/com/hahnsoftware/rodrigojt/restapi/model/GroupRepository.java
+++ backend/src/main/java/com/hahnsoftware/rodrigojt/restapi/model/GroupRepository.java
@@ -1,7 +1,6 @@
 package com.hahnsoftware.rodrigojt.restapi.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
